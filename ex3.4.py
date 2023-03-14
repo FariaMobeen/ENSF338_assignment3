c431eb3499037abdd06ed367914ac1bda384555c
FariaMobeen <113133002+FariaMobeen@users.noreply.github.com>
2023-03-14 16:03:55 -0600

    Update ex3.4.py

--- ex3.4.py
+++ ex3.4.py
@@ -3,6 +3,7 @@ import threading
 import random
 import time
 
+
 class CircularQueue:
     def __init__(self, size):
         self.size = size
@@ -10,14 +11,17 @@ class CircularQueue:
         self.head = self.tail = -1
         self._lock = threading.Lock()
 
+
     def lock(self):
         self._lock.acquire()
+
 
     def unlock(self):
         self._lock.release()
 
+
     def enqueue(self, data):
-    
+   
         while True:
             self.lock()
             if (self.head == 0 and self.tail == self.size-1) or (self.tail == self.head-1):
@@ -25,7 +29,7 @@ class CircularQueue:
                 self.unlock()
                 time.sleep(1)
                 continue
-            
+           
             if self.head == -1:
                 # Empty queue
                 self.head = self.tail = 0
@@ -33,14 +37,15 @@ class CircularQueue:
                 self.tail = 0
             else:
                 self.tail += 1
-                
+               
             self.queue[self.tail] = data
             self.unlock()
             break
 
-    
+
+   
     def dequeue(self):
-        
+       
         while True:
             self.lock()
             if self.head == -1:
@@ -48,10 +53,10 @@ class CircularQueue:
                 self.unlock()
                 time.sleep(1)
                 continue
-            
+           
             data = self.queue[self.head]
             self.queue[self.head] = None
-            
+           
             if self.head == self.tail:
                 # Queue becomes empty
                 self.head = self.tail = -1
@@ -59,9 +64,11 @@ class CircularQueue:
                 self.head = 0
             else:
                 self.head += 1
-                
+               
             self.unlock()
             return data
+
+
 
 
 def producer():
@@ -71,12 +78,14 @@ def producer():
         q.enqueue(num)
      
 
+
 def consumer():
     while True:
         num = random.randint(1, 10)
         time.sleep(num)
         data = q.dequeue()
         print(f"Consumed {data}")
+
 
 if __name__ == '__main__':
     q = CircularQueue(5)
@@ -86,3 +95,5 @@ if __name__ == '__main__':
     t2.start()
     t1.join()
     t2.join()
+
+
